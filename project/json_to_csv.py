008e15623d8d177b4dc5a063627fbfb062c8032b
pyb0924 <pyb0924@sjtu.edu.cn>
2020-12-22 17:18:27 +0800

    report finished

--- project/json_to_csv.py
+++ project/json_to_csv.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
 
     df=df.groupby([df['model'],df['problem-type']]).mean()
 
-    col = ['valid_loss', 'iou', 'dice','iou_binary','iou_parts','iou_instruments']
+    col = ['valid_loss', 'iou', 'dice','iou_binary','iou_parts','iou_instruments','dice_binary','dice_parts','dice_instruments']
     df = df[col]
 
     df.to_csv('result.csv')
--- project/visualize.py
+++ project/visualize.py
@@ -1,21 +1,27 @@
 from dataset import load_image, load_all_mask
 import torch
+from torch import nn
 from pathlib import Path
 from albumentations.pytorch.functional import img_to_tensor
 from models.models import UNet11, UNet16, UNet, LinkNet34, TDSNet
 import matplotlib.pyplot as plt
 import cv2
 
+
+def cuda(x):
+    return x.cuda() if torch.cuda.is_available() else x
+
+
 moddel_list = {
     'UNet11': UNet11,
     'UNet16': UNet16,
     'UNet': UNet,
     'LinkNet34': LinkNet34,
-    #'TDSNet': TDSNet
+    'TDSNet': TDSNet
 }
 type_list = ['binary', 'parts', 'instruments']
 
-img_file_name = 'cropped_train/instrument_dataset_8/images/frame000.jpg'
+img_file_name = 'cropped_train/instrument_dataset_5/images/frame085.jpg'
 
 if __name__ == '__main__':
 
@@ -48,49 +54,60 @@ if __name__ == '__main__':
                     num_classes = 8
                 model_name = moddel_list[moddel]
                 model = model_name(num_classes=num_classes)
-                model_path = Path('runs') / '{model}_{type}'.format(model=moddel, type=problem_type) / 'model_3.pt'
+                if torch.cuda.is_available():
+                    device_ids = list(map(int, '0'))
+                    model = nn.DataParallel(model, device_ids=device_ids).cuda()
+                model_path = Path('runs') / '{model}_{type}'.format(model=moddel, type=problem_type) / 'model_0.pt'
                 print(model_path)
                 if model_path.exists():
                     state = torch.load(str(model_path))
-                    model.load_state_dict(state['model'], strict=False)
-
-                with torch.no_grad():
-                    #model.train()
-                    model.eval()
-                    output = model(image)
-                    if problem_type == 'binary':
-                        output = (output > 0).float()
-                        output = output.data.detach().numpy()
-                        output = output[0]
-                    else:
-                        output = output.data.detach().numpy().argmax(axis=1)
+                    model.load_state_dict(state['model'])
+                # model.train()
+                model.eval()
+                image = cuda(image)
+                output = model(image)
+                if problem_type == 'binary':
+                    output = (output > 0).float()
+                    output = output.data.cpu().numpy()
+                    output = output[0]
+                else:
+                    output = output.data.cpu().numpy().argmax(axis=1)
 
                 ax = axes[j, i + 1]
                 ax.imshow(output[0], cmap='gray')
                 ax.axis('off')
-
         else:
             model = TDSNet(num_classes=8)
-            model_path = Path('runs') / 'TDSNet' / 'model_{0}.pt'
+            if torch.cuda.is_available():
+                device_ids = list(map(int, '0'))
+                model = nn.DataParallel(model, device_ids=device_ids).cuda()
+
+            model_path = Path('runs') / 'TDSNet' / 'model_0.pt'
             if model_path.exists():
                 state = torch.load(str(model_path))
                 model.load_state_dict(state['model'])
 
-                with torch.no_grad():
-                    model.eval()
-                    output_binary, output_parts, output_instruments = model(image)
+                model.eval()
+                image = cuda(image)
+                output_binary, output_parts, output_instruments = model(image)
 
-                    output_binary = torch.squeeze(output_binary).detach().numpy()
-                    output_parts = torch.squeeze(output_parts).detach().numpy()
-                    output_instruments = torch.squeeze(output_instruments).detach().numpy()
+                output_binary = (output_binary > 0).float()
+                output_binary = output_binary.data.cpu().numpy()
+                output_binary = output_binary[0][0]
 
-                ax = axes[0, i + 1]
+                output_parts = output_parts.data.cpu().numpy().argmax(axis=1)
+                output_parts = output_parts[0]
+                output_instruments = output_instruments.data.cpu().numpy().argmax(axis=1)
+                output_instruments = output_instruments[0]
+
+                ax = axes[0, 5]
                 ax.imshow(output_binary, cmap='gray')
                 ax.axis('off')
-                ax = axes[1, i + 1]
+                ax = axes[1, 5]
                 ax.imshow(output_parts, cmap='gray')
                 ax.axis('off')
-                ax = axes[2, i + 1]
+                ax = axes[2, 5]
                 ax.imshow(output_instruments, cmap='gray')
                 ax.axis('off')
+    plt.savefig('visualize.png',dpi=600)
     plt.show()
